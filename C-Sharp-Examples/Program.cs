5ec2c896723bee46a02bf9782df6f02a02f9bd32
Tahir Alvi <engtahiralvi@gmail.com>
2021-10-04 23:29:42 +0500

    Add project files.

---
+++ C-Sharp-Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
