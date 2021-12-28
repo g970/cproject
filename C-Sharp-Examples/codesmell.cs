6d6c819806a17ccb815dfefae87fb5518ae5f87d
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2021-12-28 22:22:50 +0530

    Create codesmell.cs

---
+++ C-Sharp-Examples/codesmell.cs
@@ -0,0 +1,21 @@
+bool isTrue= true;  
+  
+if(isTrue == true)  
+{  
+      // Code  
+}  
+  
+if(isTrue == false)  
+{  
+     // code  
+}  
+  
+bool IsBoolMethod()  
+{  
+    return true;  
+}  
+  
+if(IsBoolMethod () == false)  
+{  
+     // code  
+}  
