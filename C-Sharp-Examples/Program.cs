15374be48ddba0f5cf8a11edc8d18eeec4a0e2d1
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-03 00:08:29 +0530

    Update Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -29,6 +29,20 @@ namespace C_Sharp_Examples
 
             target =- num;  // Noncompliant; target = -3. Is that really what's meant?
             target =+ num; // Noncompliant; target = 3
+            
+            public void Hash(string password)
+                {
+                    var salt = Encoding.UTF8.GetBytes("Hardcoded salt");
+                    var fromHardcoded = new Rfc2898DeriveBytes(password, salt);     // Noncompliant, salt is hardcoded
+
+                    salt = Encoding.UTF8.GetBytes(password);
+                    var fromPassword = new Rfc2898DeriveBytes(password, salt);     // Noncompliant, password should not be used as a salt as it makes it predictable
+
+                    var shortSalt = new byte[8];
+                    RandomNumberGenerator.Create().GetBytes(shortSalt);
+                    var fromShort = new Rfc2898DeriveBytes(password, shortSalt);   // Noncompliant, salt is too short (should be at least 16 bytes, not 8)
+                }
+
 
         }
     }
