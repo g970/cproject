74f1e9430fd58ce1a08a9560867374973e4fa4de
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-02 23:38:11 +0530

    Update Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -7,6 +7,7 @@ namespace C_Sharp_Examples
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("so far, so good...");
         }
     }
 }
