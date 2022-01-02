99e2c62930246bd924c8a0860511a9d42fa9bce2
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-02 23:54:17 +0530

    Update Foo.cs

--- C-Sharp-Examples/Foo.cs
+++ C-Sharp-Examples/Foo.cs
@@ -1,32 +1,15 @@
 using System;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Security;
-using System.Security.Permissions;
 
-[assembly: AllowPartiallyTrustedCallersAttribute()]
 namespace MyLibrary
 {
-    [Serializable]
-    public class Foo : ISerializable
+
+    [SecurityCritical]
+    public class Foo
     {
-        private int n;
-
-        [FileIOPermissionAttribute(SecurityAction.Demand, Unrestricted = true)]
-        public Foo()
+        [SecuritySafeCritical] // Noncompliant
+        public void Bar()
         {
-           n = -1;
-        }
-
-        protected Foo(SerializationInfo info, StreamingContext context) // Noncompliant
-        {
-           n = (int)info.GetValue("n", typeof(int));
-        }
-
-        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-           info.AddValue("n", n);
         }
     }
 }
