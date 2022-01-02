b1281b97207cd6537dcaa0522fcd20324a64e655
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-02 22:41:59 +0530

    Create codesmell.cs

---
+++ C-Sharp-Examples/codesmell.cs
@@ -0,0 +1,23 @@
+1 lines (17 sloc)  226 Bytes
+   
+bool isTrue= true;  
+  
+if(isTrue == true)  
+{  
+      // Code  
+}  
+  
+if(isTrue == false)  
+{  
+     // code  
+}  
+  
+bool IsBoolMethod()  
+{  
+    return true;  
+}  
+  
+if(IsBoolMethod () == false)  
+{  
+     // code  
+}  
