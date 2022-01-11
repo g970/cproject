e42e4c68384fcf3b661eb34d29885988986c3fc1
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-11 18:32:13 +0530

    Create Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -7,6 +7,11 @@ namespace C_Sharp_Examples
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+             int target = -5;
+            int num = 3;
+
+            target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+            target =+ num; // Noncompliant; target = 3
         }
     }
 }
