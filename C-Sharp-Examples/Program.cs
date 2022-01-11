6c0be3356cf478a88829b2e90eee1b08f923d21d
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-11 19:01:06 +0530

    Update Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -4,9 +4,19 @@ namespace C_Sharp_Examples
 {
     class Program
     {
-        static void Main(string[] args):
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            public enum RpcAuthnLevel
+{
+	Default = 0,
+	None = 1,
+	Connect = 2,
+	Call = 3,
+	Pkt = 4,
+	PktIntegrity = 5,
+	PktPrivacy = 6
+}
              int target = -5;
             int num = 3;
             target =- num;  // Noncompliant; target = -3. Is that really what's meant?
