e3a1be0c89341797d70a044d79525682ef09db26
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-11 18:05:42 +0530

    Update Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
+
 namespace C_Sharp_Examples
 {
     class Program
@@ -10,15 +7,15 @@ namespace C_Sharp_Examples
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-          
+            
+            
              int target = -5;
             int num = 3;
 
             target =- num;  // Noncompliant; target = -3. Is that really what's meant?
             target =+ num; // Noncompliant; target = 3
-            
-            
-
+          
         }
     }
 }
+             
