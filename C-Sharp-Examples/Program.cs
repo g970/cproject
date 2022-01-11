e913ba68435ecd8ccc5d3b200564715b8ae90240
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-11 18:37:23 +0530

    Update Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -9,26 +9,15 @@ namespace C_Sharp_Examples
             Console.WriteLine("Hello World!");
              int target = -5;
             int num = 3;
-
             target =- num;  // Noncompliant; target = -3. Is that really what's meant?
             target =+ num; // Noncompliant; target = 3
             
-            using System;
-           using System.Security;
-
-            namespace MyLibrary
-           {
-
-          [SecurityCritical]
-          public class Foo
-        {
-        [SecuritySafeCritical] // Noncompliant
-        public void Bar()
-        {
-        }
-    }
-}
-
+    <?xml version="1.0" encoding="utf-8"?>
+<configuration>
+  <connectionStrings>
+    <add name="myConnection" connectionString="Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=" /> <!-- Noncompliant -->
+  </connectionStrings>
+</configuration>
         }
     }
 }
