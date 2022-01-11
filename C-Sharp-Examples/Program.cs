8c96b49f261711810ed27519fd4f9e565124be3a
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-01-11 18:01:55 +0530

    Update Program.cs

--- C-Sharp-Examples/Program.cs
+++ C-Sharp-Examples/Program.cs
@@ -10,21 +10,8 @@ namespace C_Sharp_Examples
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //var myBinaryFormatter = new BinaryFormatter();
-            //myBinaryFormatter.Deserialize(stream); // Noncompliant: a binder is not used to limit types during deserialization
-//             var decodedtoken1 = decoder.Decode(token, secret, verify: false); // Noncompliant: signature should be verified
-
-//             var decodedtoken2 = new JwtBuilder()
-//                .WithSecret(secret)
-//                .Decode(forgedtoken1); // Noncompliant: signature should be verified        
-            var tempPath = Path.GetTempFileName();  // Noncompliant
-
-            using (var writer = new StreamWriter(tempPath))
-            {
-                writer.WriteLine("content");
-            }
-            
-            int target = -5;
+          
+             int target = -5;
             int num = 3;
 
             target =- num;  // Noncompliant; target = -3. Is that really what's meant?
